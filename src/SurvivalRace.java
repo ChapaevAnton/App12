c57e02cd117e043371cb24d7d276cc1117eeddc3
ChapaevAnton <virsus2009@gmail.com>
2021-03-23 13:27:42 +0700

    12.6

--- src/SurvivalRace.java
+++ src/SurvivalRace.java
@@ -5,38 +5,61 @@ import java.util.List;
 public class SurvivalRace {
 
 
-    static final int SIZE = 100_000_000;
+    static final int SIZE = 10_000_000;
 
     public static void main(String[] args) {
-        long time_start = System.currentTimeMillis();
 
         List<Integer> maxList = new ArrayList<>(SIZE);
         List<Integer> sortList = new ArrayList<>(SIZE);
         List<Integer> lastRemoveList = new ArrayList<>(SIZE);
 
+        Runnable run1 = () -> {
+            for (int i = 0; i < SIZE; i++) {
+                maxList.add(i);
+            }
 
-        for (int i = 0; i < SIZE; i++) {
-            maxList.add(i);
+            int max = Collections.max(maxList);
+            System.out.println("max ready: " + max);
+        };
+
+
+        Runnable run2 = () -> {
+            for (int i = SIZE; i > 0; i--) {
+                sortList.add(i);
+            }
+
+            Collections.sort(sortList);
+            System.out.println("sort ready...");
+        };
+
+        Runnable run3 = () -> {
+            for (int i = 0; i < SIZE; i++) {
+                lastRemoveList.add(i);
+            }
+
+            while (lastRemoveList.size() != 0) {
+                lastRemoveList.remove(lastRemoveList.size() - 1);
+            }
+            System.out.println("clear ready...");
+        };
+
+        ThreadGroup threadGroup = new ThreadGroup("threadAll");
+        Thread[] threads = {new Thread(threadGroup, run1), new Thread(threadGroup, run2), new Thread(threadGroup, run3)};
+        //threadGroup.setMaxPriority(Thread.MAX_PRIORITY);
+
+        long time_start = System.currentTimeMillis();
+
+        for (Thread thread : threads) {
+            thread.start();
         }
 
-        int max = Collections.max(maxList);
-        System.out.println("max ready: " + max);
-
-
-        for (int i = SIZE; i > 0; i--) {
-            sortList.add(i);
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException err) {
+                System.err.println(err.getMessage());
+            }
         }
-        Collections.sort(sortList);
-        System.out.println("sort ready...");
-
-        for (int i = 0; i < SIZE; i++) {
-            lastRemoveList.add(i);
-        }
-
-        while (lastRemoveList.size() != 0) {
-            lastRemoveList.remove(lastRemoveList.size() - 1);
-        }
-        System.out.println("clear ready...");
 
 
         long time_stop = System.currentTimeMillis();
