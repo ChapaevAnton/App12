95147bf5b249a53eae03a8bcc645f8ec49ff0611
Chapaev Anton <virsus2009@gmail.com>
2021-03-28 17:03:28 +0700

    up 12.5

--- src/test/A.java
+++ src/test/A.java
@@ -1,26 +1,62 @@
 package test;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class A {
 
     public static void main(String[] args) {
 
-        int result = 0;
 
         try (FileReader fileReader = new FileReader("c:\\TEMP\\input.txt");
              FileWriter fileWriter = new FileWriter("c:\\TEMP\\output.txt", false)) {
 
             Scanner in = new Scanner(new BufferedReader(fileReader));
 
-            String[] array = in.nextLine().split(" ");
+            ArrayList<String> arrayList = new ArrayList<>();
 
-            for (int i = 0; i < array.length; i++) {
-                result += Integer.parseInt(array[i]);
+
+            while (in.hasNextLine()) {
+                String str = in.nextLine();
+                arrayList.add(str);
+
             }
 
-            fileWriter.write(Integer.toString(result));
+            TreeMap<Integer, String> treeMap = new TreeMap<>();
+
+            for (String str : arrayList) {
+
+                for (int i = 0; i < str.length(); i++) {
+                    if (Character.isDigit(str.charAt(i))) {
+
+                        int count = Integer.parseInt(String.valueOf(str.charAt(i)));
+
+                        str = str.replaceAll(Character.toString(str.charAt(i)), "");
+
+                        treeMap.put(count, str);
+                    }
+
+                }
+
+            }
+
+            int mapSize = treeMap.size() - 1;
+
+            arrayList = new ArrayList<>(treeMap.values());
+
+            for (int i = 0; i < arrayList.size(); i++) {
+
+                fileWriter.write(arrayList.get(i));
+                if (i < mapSize) {
+                    fileWriter.append('\n');
+                }
+            }
+
 
             fileWriter.flush();
 
